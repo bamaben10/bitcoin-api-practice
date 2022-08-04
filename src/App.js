0b9a1267ead03585cdcf464011210f312d2800eb
Benjamin Salmon <bensalmon8@gmail.com>
2022-08-04 16:58:54 -0400

    debugging

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import Coin from "./Coin";
 function App() {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
-
+  console.log(coins);
   useEffect(() => {
     axios
       .get(
-        "http://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
       )
       .then((res) => {
         setCoins(res.data);
